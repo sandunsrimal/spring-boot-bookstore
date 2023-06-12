86392b61965049249b9e9b5a5a2378b136292df0
sandunsrimal <bandara-cs19005@stu.kln.ac.lk>
2023-06-12 19:49:05 +0530

    first commit

---
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.example.bookstore.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.bookstore.entity.Book;
+import com.example.bookstore.service.BookService;
+
+@Controller
+public class BookController {
+    @Autowired
+    private BookService service;
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+    @GetMapping("/book_register")
+    public String bookRegister() {
+        return "bookRegister";
+    }
+    @GetMapping("/book_list")
+    public ModelAndView getAllBooks() {
+        List<Book> list = service.getAllBooks();
+        // ModelAndView m= new ModelAndView();
+        // m.setViewName("bookList");
+        // m.addObject("book",list);
+        return new ModelAndView("bookList", "book", list);
+    }
+
+    @PostMapping("/save")
+    public String addBook(@ModelAttribute Book b) {
+        service.save(b);
+        return "redirect:/book_list";
+    }
+}
---
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,22 @@
+package com.example.bookstore.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.bookstore.entity.Book;
+import com.example.bookstore.repository.BookRepository;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bRepo;
+    public void save(Book b){
+        bRepo.save(b);
+    }
+
+    public List<Book> getAllBooks(){
+        return bRepo.findAll();
+    }
+}
